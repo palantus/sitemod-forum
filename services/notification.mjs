999e00fa1a66a232c2671d51a4b13155efeb7620
Anders Houbak Kristiansen <palantus@gmail.com>
2022-11-03 10:52:28 +0100

    Update usage of mail

--- services/notification.mjs
+++ services/notification.mjs
@@ -1,6 +1,6 @@
 import CoreSetup from "../../../models/setup.mjs"
 import LogEntry from "../../../models/logentry.mjs"
-import MailSender from "../../mail/services/mailsender.mjs"
+import Mail from "../../mail/models/mail.mjs"
 import { query } from "entitystorage";
 import User from "../../../models/user.mjs";
 
@@ -9,22 +9,18 @@ export async function sendMailsNewThread(thread){
   for(let user of query.tag("user").relatedTo(query.prop("emailMeOnForumUpdates", true)).relatedTo(query.prop("notifyAllNewThreads", true)).all){
     if(!user.email) continue;
     if(user.id == thread.related.owner?.id) continue; // No need to notify author
-    try{
-      await new MailSender().send({
-        to: user.email, 
-        subject: `${CoreSetup.lookup().siteTitle}: New thread on forums`, 
-        body: `
-          <div>
-            <h3>Hello ${user.name}</h3>
-            <p>A new thread has been posted in the forums by ${thread.author.name}:</p>
-            <a href="${global.sitecore.siteURL}/forum/thread/${thread.id}">${thread.title}</a>
-          </div>
-        `,
-        bodyType: "html"
-      })
-    } catch(err){
-      new LogEntry(`Could not send email to ${user.email}. Error: ${err}`, "forum")
-    }
+    await new Mail({
+      to: user.email, 
+      subject: `${CoreSetup.lookup().siteTitle}: New thread on forums`, 
+      body: `
+        <div>
+          <h3>Hello ${user.name}</h3>
+          <p>A new thread has been posted in the forums by ${thread.author.name}:</p>
+          <a href="${global.sitecore.siteURL}/forum/thread/${thread.id}">${thread.title}</a>
+        </div>
+      `,
+      bodyType: "html"
+    }).send()
   }
 }
 
@@ -43,27 +39,23 @@ export async function sendMailsNewPosts(thread, post){
     if(user.setup.notifyForumUpdates === false) continue;
     if(!thread.rels.subscribee?.find(u => u.id == user.id)) continue; // Not subscribed to thread
     if(!user.permissions.includes("forum.read")) continue; //No need to notify someone who can't read it
-    try{
-      await new MailSender().send({
-        to: user.email, 
-        subject: `${CoreSetup.lookup().siteTitle}: New reply on forums`, 
-        body: `
+    await new Mail({
+      to: user.email, 
+      subject: `${CoreSetup.lookup().siteTitle}: New reply on forums`, 
+      body: `
+        <div>
+          <h3>Hello ${user.name}</h3>
           <div>
-            <h3>Hello ${user.name}</h3>
-            <div>
-              <p>A new reply by ${post.author.name} has been posted to the following thread:</p>
-              <a href="${global.sitecore.siteURL}/forum/thread/${thread.id}">${thread.title}</a>
-            </div>
-            <br>
-            <hr>
-            <p style="font-size: 10pt;">If you do not want to receive these types of emails, you can unsubscribe <a href="${global.sitecore.siteURL}/profile">here</a></p>
+            <p>A new reply by ${post.author.name} has been posted to the following thread:</p>
+            <a href="${global.sitecore.siteURL}/forum/thread/${thread.id}">${thread.title}</a>
           </div>
-        `,
-        bodyType: "html"
-      })
-    } catch(err){
-      new LogEntry(`Could not send email to ${user.email}. Error: ${err}`, "forum")
-    }
+          <br>
+          <hr>
+          <p style="font-size: 10pt;">If you do not want to receive these types of emails, you can unsubscribe <a href="${global.sitecore.siteURL}/profile">here</a></p>
+        </div>
+      `,
+      bodyType: "html"
+    }).send()
   }
 }
 
